814da629cff0c05a613b19800e5cdc1193105f73
sonamahapatra <sonalimahapatra105@gmail.com>
2023-04-13 13:36:08 +0530

    [sonamahapatra] Added QuotientandRemainder Program

--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -12,7 +12,9 @@ namespace BasicCoreProgram
         {
             Console.WriteLine("Welcome To The Basic Core Program");
             Console.WriteLine("Please Choose One From Below Option: ");
-            Console.WriteLine("1.FlipCoin\n 2.LeapYear\n 3.PowerOfTwo \n 4.Harmonic Series \n 5.Factor");
+            Console.WriteLine("1.FlipCoin\n 2.LeapYear\n 3.PowerOfTwo \n 4.Harmonic Series \n 5.Factor \n 6.FindQuotientAndRemainder");
+       
+                
             int Option = Convert.ToInt32(Console.ReadLine());
             switch (Option)
             {
@@ -30,6 +32,9 @@ namespace BasicCoreProgram
                     break;
                     case 5:
                         Factor.PrimeFactor();
+                    break;
+                    case 6:
+                    QuotientandRemainder.FindQuotientAndRemainder();
                     break;
                 default:
                     Console.WriteLine("Please Choose Program With Given Option");
---
+++ BasicCoreProgram/QuotientandRemainder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class QuotientandRemainder
+    {
+        public static void FindQuotientAndRemainder()
+        {
+            int dividend = 50, divisor = 8;
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Dividend : {0} Divisor: {1}", dividend, divisor);
+            // Console.WriteLine("Dividend : " + dividend+" Divisor: " + divisor);
+
+            Console.WriteLine("Quotient =  " + quotient);
+            Console.WriteLine("Remainder =  " + remainder);
+        }
+    }
+}
