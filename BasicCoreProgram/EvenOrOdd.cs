abb29cdd04241ae2d4c25337c74ec6a2e4ec6551
sonamahapatra <sonalimahapatra105@gmail.com>
2023-04-13 14:00:25 +0530

    [sonamahapatra] Checking The number is Even or odd

---
+++ BasicCoreProgram/EvenOrOdd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class EvenOrOdd
+    {
+        public static void FindEvenOrOdd()
+        {
+            int i;
+            Console.WriteLine("Input a Number: ");
+            i = int.Parse(Console.ReadLine());
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("The number is even");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd");
+            }
+        }
+    }
+}
--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -12,7 +12,7 @@ namespace BasicCoreProgram
         {
             Console.WriteLine("Welcome To The Basic Core Program");
             Console.WriteLine("Please Choose One From Below Option: ");
-            Console.WriteLine("1.FlipCoin\n 2.LeapYear\n 3.PowerOfTwo \n 4.Harmonic Series \n 5.Factor \n 6.FindQuotientAndRemainder \n 7.Swap");
+            Console.WriteLine("1.FlipCoin\n 2.LeapYear\n 3.PowerOfTwo \n 4.Harmonic Series \n 5.Factor \n 6.FindQuotientAndRemainder \n 7.Swap \n 8.EvenOrOdd");
        
                 
             int Option = Convert.ToInt32(Console.ReadLine());
@@ -38,6 +38,9 @@ namespace BasicCoreProgram
                     break;
                 case 7:
                     Swap.SwapNumbers();
+                    break;
+                    case 8:
+                        EvenOrOdd.FindEvenOrOdd();
                     break;
                 default:
                     Console.WriteLine("Please Choose Program With Given Option");
