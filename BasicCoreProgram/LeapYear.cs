b0eedabee0c87bfce8404314027a3b8d97019b74
sonamahapatra <sonalimahapatra105@gmail.com>
2023-04-13 12:23:38 +0530

    [sonamahapatra] Added LeapYear Program

---
+++ BasicCoreProgram/LeapYear.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class LeapYear
+    {
+        public static void TheLeapYear()
+        {
+            System.Console.WriteLine("Enter the Year in four digits : ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+            {
+                Console.WriteLine(" {0} is a Leap Year.", year);
+            }
+            else
+            {
+                Console.WriteLine(" {0} is  not a Leap Year.", year);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -11,7 +11,23 @@ namespace BasicCoreProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To The Basic Core Program");
-            FlipCoin.Flip();
+            Console.WriteLine("Please Choose One From Below Option: ");
+            Console.WriteLine("1.FlipCoin\n 2.LeapYear\n");
+            int Option = Convert.ToInt32(Console.ReadLine());
+            switch (Option)
+            {
+                case 1:
+                    FlipCoin.Flip();
+                    break;
+                case 2:
+                    LeapYear.TheLeapYear();
+                    break;
+                default:
+                    Console.WriteLine("Please Choose Program With Given Option");
+                    break;
+            }
+
+
         }
     }
 }
